c05174a7a4496e186ed11514d1f035a9dbb3fd53
Christiaan Swanepoel <christiaan@myhr.works>
2025-10-27 13:53:51 +1300

    Static icons

--- frontend/components/map/features/SelectablePointLineComponent.tsx
+++ frontend/components/map/features/SelectablePointLineComponent.tsx
@@ -4,6 +4,30 @@ import { PolylineFromGpx } from "../PolylineFromGpx";
 import { Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 
+function getIcon(iconType: "default" | "selected" = "default"): L.Icon {
+  switch (iconType) {
+    case "selected":
+      return L.icon({
+        iconUrl: "/markers/selected-marker-32.svg",
+        iconSize: [32, 32],
+      });
+    case "default":
+      return L.icon({
+        iconUrl: "/markers/marker-32.svg",
+        iconSize: [32, 32],
+      });
+    default:
+      return L.icon({
+        iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowUrl:
+          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+      });
+  }
+}
+
 export function SelectablePointLineComponent({
   onPointSelect,
   points,
@@ -18,18 +42,8 @@ export function SelectablePointLineComponent({
           eventHandlers={{
             click: () => onPointSelect?.(pt, idx),
           }}
-          icon={L.icon({
-            iconUrl:
-              "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowUrl:
-              "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-          })}
-        >
-          <Popup>Point {idx + 1}</Popup>
-        </Marker>
+          icon={getIcon()}
+        />
       ))}
     </>
   );
